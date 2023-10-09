995e05259a19fe152bdc0d74a24062436967daf7
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-10-09 15:03:38 +0200

    P1_1ClassesGuessThatsCorrect

--- P1_1Classes/Program.cs
+++ P1_1Classes/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Car car = new Car();
+
+Animal animal = new Animal();
+
+Person person = new Person();
+
+Console.WriteLine(person);
+Console.WriteLine(animal);
+Console.WriteLine(car);
