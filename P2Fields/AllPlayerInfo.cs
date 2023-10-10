e266875b889202d4e18d70de913997409334fc50
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-10-10 14:33:54 +0200

    P2_1PlayerGoldMade

---
+++ P2Fields/AllPlayerInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace P2Fields;
+
+public class AllPlayerInfo
+{
+    public int Gold;
+    public int Wood;
+    public int Stone;
+    public int Level;
+    public int Experience;
+    public bool VIPSubscription;
+    public bool PVP;
+    public string Name;
+    public string Email;
+}
---
+++ P2Fields/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System.ComponentModel.Design;
+using P2Fields;
+
+AllPlayerInfo player = new AllPlayerInfo();
+
+player.Name = "Marina";
+player.Email = "marina.eleni.voukouresli@student.forsbergsskola.se";
+player.Gold = 200;
+player.Stone = 110;
+player.Wood = 53;
+player.Level = 12;
+player.Experience = 123;
+
+player.VIPSubscription = true;
+player.PVP = false;
+
+Console.WriteLine("Name: " + player.Name);
+Console.WriteLine("E-mail addrerss: " + player.Email);
+Console.WriteLine("Player A has " + player.Gold + "Gold, " + player.Stone+"Stone, "
+    + player.Wood+"Wood. He is level " + player.Level + " and has experience " + player.Experience+"." +
+     player.VIPSubscription,  player.PVP);
+
