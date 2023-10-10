a3e027ca2fe9c5eb2f2283113847c5b217d4d730
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-10-10 11:44:56 +0200

    SoeCommentsItemsValue

--- P2_2ItemValues/Program.cs
+++ P2_2ItemValues/Program.cs
@@ -6,10 +6,10 @@ Items [] items = new Items[3];
 
 for (var i = 0; i < items.Length; i++)
 {
-    items[i] = new Items();
+    items[i] = new Items();//assign the new itmes first to items
 }
 
-items[0].GoldValue =  100;
+items[0].GoldValue =  100;//assign a balue to them
 items[1].GoldValue =  200;
 items[2].GoldValue =  300;
 
