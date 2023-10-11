41a25581d9650528afe5ff378038ec47ad7f0c21
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-10-11 09:59:50 +0200

    People2Made

---
+++ P3_2People/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace P3_2People;
+
+public class Person
+{
+    public string? name;
+
+    public void Greeting()
+    {
+        Console.WriteLine("Hello, " + name);
+    }
+}
---
+++ P3_2People/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+using P3_2People;
+
+Person[] people = new Person[3];
+
+
+for (var i = 0; i < people.Length; i++)
+{
+   people[i] = new Person();
+   Console.WriteLine("What name would you like to give?");
+   string userInput= Console.ReadLine();
+   people[i].name = userInput;
+
+}
+ 
+for (var i = 0; i < people.Length; i++)
+{ 
+   people[i].Greeting();
+}
