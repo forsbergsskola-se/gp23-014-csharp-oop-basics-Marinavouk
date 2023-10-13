0bd1cb5bc73ee80eae1ecfff5192cdbc561ae12d
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-10-13 12:48:56 +0200

    CompositionIsNotReallyWorking

--- Exercises 6 - Composition/Knight.cs
+++ Exercises 6 - Composition/Knight.cs
@@ -1,19 +1,39 @@
 ﻿
 
 
-public class Knight
+class Knight
 {
-    public Shield Shield;
-    public Sword Sword;
+    public  Shield Shield;
+    public  Sword Sword;
 
-    public void GetHit(Sword Sword)
+    public void GetHit(Sword sword)
     {
-        
+        if (Shield != null)
+        {
+            Console.WriteLine("Knights Shield fends off the strike.");
+            Shield = null;
+        }
+        else
+        {
+            Console.WriteLine("The Knight has no Shield and is wounded.");
+        }
     }
 
-    public void Attack(Knight Knight)
+    public void Attack(Knight shield)
     {
-        
+        if (Sword == null)
+        {
+           
+           Console.WriteLine("Knight attacks Knight with a Sword.");
+            shield.GetHit(Sword);
+            if (Shield != null) 
+                Console.WriteLine("Knights Shield was pierced by the Sword.");
+        }
+        else
+        {
+            Console.WriteLine("Knight attacks with Bare Hands.");
+            shield.GetHit(null);
+        }   
     }
-
 }
+
--- Exercises 6 - Composition/Program.cs
+++ Exercises 6 - Composition/Program.cs
@@ -2,7 +2,25 @@
 
 Knight defender = new Knight();
 Knight attacker = new Knight();
-
 defender.Shield = new Shield();
-
 attacker.Sword = new Sword();
+
+
+attacker.Attack(defender);
+
+
+
+attacker.Attack(defender);
+Console.WriteLine("Equipping Sword...");
+attacker.Attack(defender);
+attacker.Attack(defender);
+
+
+
+
+
+
+
+
+
+
